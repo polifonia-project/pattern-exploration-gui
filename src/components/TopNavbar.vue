33ebd2411782d3329790d394e0a1ca577ffd9b25
Rory Sweeney <r.sweeney4@universityofgalway.ie>
2024-02-27 17:37:39 +0000

    Changed html link elements to router-link elements.

--- src/components/TopNavbar.vue
+++ src/components/TopNavbar.vue
@@ -3,19 +3,19 @@
         <div class="container-fluid">
             <!-- a class="navbar-brand" href="/">Polifonia</a -->
 
-            <a href="/" class="navbar-brand">
+            <router-link to="/" class="navbar-brand">
                 <img src="@/assets/logo-polifonia.svg" alt="">
-            </a>
-            <a href="/" class="navbar-brand align-bottom pl-3 pe-4 pb-0 pt-2">
+            </router-link>
+            <router-link to="/" class="navbar-brand align-bottom pl-3 pe-4 pb-0 pt-2">
                 <span id="site_name">Patterns UI</span>
-            </a>
+            </router-link>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="/">Search</a>
+                        <router-link to="/" class="nav-link active" aria-current="page">Search</router-link>
                     </li>
                     <li class="nav-item">
                         <router-link class="nav-link" to="/about">About</router-link>
